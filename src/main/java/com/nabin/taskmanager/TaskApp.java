df22134d6f05032879e0a2344d64f073cc06c53c
Suckscorpio999 <olinabin214@gmail.com>
2026-02-14 11:57:28 +0545

    feat- TaskApp!

--- src/main/java/com/nabin/taskmanager/TaskApp.java
+++ src/main/java/com/nabin/taskmanager/TaskApp.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TaskManagerApplication {
+public class TaskApp {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TaskManagerApplication.class, args);
+		SpringApplication.run(TaskApp.class, args);
 	}
 
 }
