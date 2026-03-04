9d58a463fcf35f0de9f532b4f4e2a656bc854e6b
Suckscorpio999 <olinabin214@gmail.com>
2026-03-04 11:13:45 +0545

    feat - added security methods

--- backend/src/main/java/com/nabin/workflow/util/SecurityUtil.java
+++ backend/src/main/java/com/nabin/workflow/util/SecurityUtil.java
@@ -6,6 +6,7 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 
+//for security
 @Component
 public class SecurityUtil {
     /**
