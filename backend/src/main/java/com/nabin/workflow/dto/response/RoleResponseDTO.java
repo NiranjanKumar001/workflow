2a13f7930816d357a96fc5c157d650670ef1512a
Suckscorpio999 <olinabin214@gmail.com>
2026-03-04 10:29:24 +0545

    feat - changed RoleDTO to meaningful RoleResponseDTO

--- backend/src/main/java/com/nabin/workflow/dto/response/RoleResponseDTO.java
+++ backend/src/main/java/com/nabin/workflow/dto/response/RoleResponseDTO.java
@@ -1,4 +1,4 @@
-package com.nabin.taskmanager.dto;
+package com.nabin.workflow.dto.response;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -9,7 +9,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-public class RoleDTO {
+public class RoleResponseDTO {
     private Long id;
     private String name;
 }
