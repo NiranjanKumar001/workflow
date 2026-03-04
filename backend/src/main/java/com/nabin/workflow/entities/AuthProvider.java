050d009a5988367c334346b5097d86f3c19b89b9
Suckscorpio999 <olinabin214@gmail.com>
2026-03-04 10:52:28 +0545

    feat - created and defined auth providers

---
+++ backend/src/main/java/com/nabin/workflow/entities/AuthProvider.java
@@ -0,0 +1,8 @@
+package com.nabin.workflow.entities;
+
+public enum AuthProvider {
+    LOCAL,      // Email/Password registration
+    GOOGLE,     // Google OAuth
+    FACEBOOK,   // Facebook OAuth (for future)
+    GITHUB      // GitHub OAuth (for future)
+}
