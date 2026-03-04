10728bfb604ec2995e8d308bf6df7a325ee00e27
Suckscorpio999 <olinabin214@gmail.com>
2026-03-04 10:54:21 +0545

    feat - created password changing config file

---
+++ backend/src/main/java/com/nabin/workflow/dto/request/ChangePasswordDTO.java
@@ -0,0 +1,28 @@
+package com.nabin.workflow.dto.request;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ChangePasswordDTO {
+
+    @NotBlank(message = "Current password is required")
+    private String currentPassword;
+
+    @NotBlank(message = "New password is required")
+    @Size(min = 6, message = "New password must be at least 6 characters")
+    @Pattern(
+            regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=]).{6,}$",
+            message = "Password must contain at least one digit, one lowercase, one uppercase, and one special character"
+    )
+    private String newPassword;
+
+    @NotBlank(message = "Confirm password is required")
+    private String confirmPassword;
+}
