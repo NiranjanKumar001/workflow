ae0d21809d0901944dad80d11eb6fc3e3b7f7a39
Suckscorpio999 <olinabin214@gmail.com>
2026-03-04 22:14:48 +0545

    feat : created reset password interface and declared some methods

---
+++ backend/src/main/java/com/nabin/workflow/repository/PasswordResetTokenRepository.java
@@ -0,0 +1,25 @@
+package com.nabin.workflow.repository;
+
+import com.nabin.workflow.entities.PasswordResetToken;
+import com.nabin.workflow.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
+
+@Repository
+public interface PasswordResetTokenRepository extends JpaRepository<PasswordResetToken, Long> {
+
+    Optional<PasswordResetToken> findByToken(String token);
+
+    @Modifying
+    @Query("DELETE FROM PasswordResetToken prt WHERE prt.user = :user")
+    void deleteByUser(User user);
+
+    @Modifying
+    @Query("DELETE FROM PasswordResetToken prt WHERE prt.expiryDate < :now")
+    void deleteExpiredTokens(LocalDateTime now);
+}
