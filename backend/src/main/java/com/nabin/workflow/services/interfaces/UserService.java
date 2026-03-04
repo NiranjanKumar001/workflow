ec9e875dbe74ee8fd4c41a975f2ea2fdcd383b0d
Suckscorpio999 <olinabin214@gmail.com>
2026-03-04 10:34:36 +0545

    feat : declared useful methods for Admin and profile management.

---
+++ backend/src/main/java/com/nabin/workflow/services/interfaces/UserService.java
@@ -0,0 +1,29 @@
+package com.nabin.workflow.services.interfaces;
+
+import com.nabin.workflow.dto.request.ChangePasswordDTO;
+import com.nabin.workflow.dto.request.UpdateProfileDTO;
+import com.nabin.workflow.dto.request.UserRegistrationDTO;
+import com.nabin.workflow.dto.response.UserProfileDTO;
+import com.nabin.workflow.dto.response.UserResponseDTO;
+import jakarta.validation.Valid;
+
+import java.util.List;
+
+public interface UserService {
+    UserResponseDTO registerUser(UserRegistrationDTO registrationDTO);
+    UserResponseDTO getUserById(Long id);
+    UserResponseDTO getUserByEmail(String email);
+    UserResponseDTO getUserByUsername(String username);
+    boolean existsByEmail(String email);
+    boolean existsByUsername(String username);
+
+    UserResponseDTO updateUser(Long userId, @Valid UserRegistrationDTO updateDTO);
+    //  Admin methods
+    List<UserResponseDTO> getAllUsers();
+    void deleteUser(Long userId);
+
+    //Profile management methods
+    UserProfileDTO getCurrentUserProfile();
+    UserProfileDTO updateCurrentUserProfile(UpdateProfileDTO updateProfileDTO);
+    void changePassword(ChangePasswordDTO changePasswordDTO);
+}
