8e12c74af096bdcd689f52393e70420a864329f9
Suckscorpio999 <olinabin214@gmail.com>
2026-03-04 10:17:09 +0545

    refactor: rename TaskApp to WorkflowApplication to match new branding

---
+++ backend/src/main/java/com/nabin/workflow/WorkFlowApplication.java
@@ -0,0 +1,16 @@
+package com.nabin.workflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@EnableScheduling
+public class WorkFlowApplication {
+
+	public static void main(String[] args)
+	{
+		SpringApplication.run(WorkFlowApplication.class, args);
+	}
+
+}
