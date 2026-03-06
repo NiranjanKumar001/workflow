cdbdf8e179facd01f24c637bf391709ad3a52793
Suckscorpio999 <olinabin214@gmail.com>
2026-03-06 10:15:10 +0545

    feat(TaskUpdateDTO.java): all fields optional for partial updates

---
+++ backend/src/main/java/com/nabin/workflow/dto/request/TaskUpdateDTO.java
@@ -0,0 +1,30 @@
+package com.nabin.workflow.dto.request;
+
+import com.nabin.workflow.entities.TaskPriority;
+import com.nabin.workflow.entities.TaskStatus;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class TaskUpdateDTO {
+
+    @Size(min = 1, max = 200, message = "Title must be between 1 and 200 characters")
+    private String title;
+
+    @Size(max = 5000, message = "Description cannot exceed 5000 characters")
+    private String description;
+
+    private TaskStatus status;
+
+    private TaskPriority priority;
+
+    private LocalDate dueDate;
+
+    private Long categoryId;
+}
