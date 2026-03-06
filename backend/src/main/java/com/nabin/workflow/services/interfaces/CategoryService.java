3289be92577d6c626b28be4540bcec31d9e8078b
Suckscorpio999 <olinabin214@gmail.com>
2026-03-06 10:17:51 +0545

    feat(CategoryService.java): add category service interface

--- backend/src/main/java/com/nabin/workflow/services/interfaces/CategoryService.java
+++ backend/src/main/java/com/nabin/workflow/services/interfaces/CategoryService.java
@@ -1,13 +1,14 @@
 package com.nabin.workflow.services.interfaces;
 
 import com.nabin.workflow.dto.request.CategoryRequestDTO;
+import com.nabin.workflow.dto.response.CategoryResponseDTO;
 
 import java.util.List;
 
 public interface CategoryService {
-    CategoryRequestDTO createCategory(Long userId, CategoryRequestDTO categoryRequestDTO);
-    CategoryRequestDTO getCategoryById(Long userId, Long categoryId);
-    List<CategoryRequestDTO> getAllCategoriesByUserId(Long userId);
-    CategoryRequestDTO updateCategory(Long userId, Long categoryId, CategoryRequestDTO categoryRequestDTO);
+    CategoryResponseDTO createCategory(Long userId, CategoryRequestDTO categoryRequestDTO);
+    CategoryResponseDTO getCategoryById(Long userId, Long categoryId);
+    List<CategoryResponseDTO> getAllCategoriesByUserId(Long userId);
+    CategoryResponseDTO updateCategory(Long userId, Long categoryId, CategoryRequestDTO categoryRequestDTO);
     void deleteCategory(Long userId, Long categoryId);
 }
